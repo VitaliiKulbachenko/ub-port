0e793f687fb06af182e61ac9db41a3d75a6b7055
Vitalii Kulbachenko <vitalii.kulbachenko@gmail.com>
2017-08-14 15:19:47 +0300

    change dev-install

--- dev-install/install.sh
+++ dev-install/install.sh
@@ -1,4 +1,21 @@
-#install vim 
-sudo apt-get install -y vim
+#update system
+sudo apt-get update && sudo apt-get upgrade
 
-#install 
+#install base
+sudo apt-get install -y git vim wget  
+
+#install Filezilla
+cd /tmp
+wget http://filezilla.ru/download/FileZilla_3.27.0.1_x86_64-linux-gnu.tar.bz2
+tar -xvjf FileZilla_3.27.0.1_x86_64-linux-gnu.tar.bz2
+sudo mv FileZilla3/ /opt/filezilla3/
+sudo ln -s /opt/filezilla3/bin/filezilla /usr/local/bin/filezilla
+
+#install PHPShtorm
+cd /tmp
+wget https://download.jetbrains.com/webide/PhpStorm-2017.2.1.tar.gz
+tar xvf PhpStorm-2017.2.1.tar.gz
+sudo mv PhpStorm-172.3544.41/ /opt/phpstorm/
+sudo ln -s /opt/phpstorm/bin/phpstorm.sh /usr/local/bin/phpstorm
+
+
