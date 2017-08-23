f63e38b5cd5b152b67d1acec96dd4d3e1ed13c08
Vitalii Kulbachenko <vitalii.kulbachenko@gmail.com>
2017-08-23 03:09:52 +0300

    add

--- functions/wine.sh
+++ functions/wine.sh
@@ -5,3 +5,6 @@ sudo apt-key add Release.key
 sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
 sudo apt-get update
 sudo apt-get install --install-recommends winehq-stable
+
+rm -rf ~/.wine
+WINEARCH=win32 WINEPREFIX=~/.wine winecfg
