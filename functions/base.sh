f427d035ee91f5d23caffdd7014c80af95350300
Vitalii Kulbachenko <vitalii.kulbachenko@gmail.com>
2017-09-06 22:30:40 +0300

    fix

--- functions/base.sh
+++ functions/base.sh
@@ -3,7 +3,7 @@
 base () {
     cp samples/bash/.bashrc $HOME
     sudo apt-get update && sudo apt-get upgrade -y
-    sudo apt-get install -y git curl wget rar vim
+    sudo apt-get install -y git curl wget rar vim gparted
     sudo apt-get install -y unity-tweak-tool
 }
 
